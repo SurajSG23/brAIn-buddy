36a323beff34e81a2d2c04b4551f8521a2a2c737
Suraj S G Dhanva <surajsgd23@gmail.com>
2025-04-28 21:02:02 +0530

    feat: add CORS support for Vercel deployment and create vercel.json for routing

--- Server/app.js
+++ Server/app.js
@@ -19,7 +19,7 @@ app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
 
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: ["http://localhost:5173","https://brainbuddy.vercel.app"],
     credentials: true,
   })
 );
