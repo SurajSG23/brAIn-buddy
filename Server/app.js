a4817e26dd331ed5de0e07f14c83b5e5ee36485d
Suraj S G Dhanva <surajsgd23@gmail.com>
2025-04-29 23:01:21 +0530

    fix: reorder middleware initialization and clean up CORS configuration

--- Server/app.js
+++ Server/app.js
@@ -8,34 +8,27 @@ import connectDB from "./config/db.js";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
 
-dotenv.config();
 connectDB();
+dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(
-  cors({
-    origin: "https://brainbuddy.vercel.app", 
-    credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], 
-    allowedHeaders: ["Content-Type", "Authorization"],
-  })
-);
-
-app.options("*", cors());
-
 app.use(bodyParser.json({ limit: "50mb" }));
 app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
 app.use(cookieParser());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+
+app.use(
+  cors({
+    origin: ["https://brainbuddy.vercel.app", "http://localhost:5173"], 
+    credentials: true, 
+  })
+);
 
 app.get("/", (req, res) => {
   res.send("API is running...");
 });
 
-// API routes
 app.use("/register", registerRouter);
 app.use("/uploadimagekit", uploadRouter);
 app.use("/project", projectRouter);
