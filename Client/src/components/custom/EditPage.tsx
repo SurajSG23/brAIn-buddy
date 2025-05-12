9d5728a68f51f8757a593021be64d16b949fb5b6
Suraj S G Dhanva <surajsgd23@gmail.com>
2025-05-12 06:16:19 +0530

    fix: remove unnecessary console log and add close project request in TestPage

--- Client/src/components/custom/EditPage.tsx
+++ Client/src/components/custom/EditPage.tsx
@@ -30,7 +30,6 @@ const EditPage = () => {
             }`,
             { withCredentials: true }
           );
-          console.log(res.data)
           await axios.post(
             `${import.meta.env.VITE_BACKEND_URL}/project/closeproject/${
               userFound.data._id
