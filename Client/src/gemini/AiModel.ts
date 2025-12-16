cc5dda30f1ba17b09e415ae3cccf7cd138facd07
Suraj S G Dhanva <surajsgd23@gmail.com>
2025-12-16 11:24:04 +0530

    Change the model

--- Client/src/gemini/AiModel.ts
+++ Client/src/gemini/AiModel.ts
@@ -5,7 +5,7 @@ const apiKey = import.meta.env.VITE_GOOGLE_AI_API_KEY;
 const genAI = new GoogleGenerativeAI(apiKey);
 
 const model = genAI.getGenerativeModel({
-  model: "gemini-2.0-flash-lite",
+  model: "gemini-2.0-flash",
 });
 
 const generationConfig = {
